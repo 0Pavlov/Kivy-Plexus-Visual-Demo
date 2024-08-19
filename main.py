eae13d4e22a633149d4eb85267619b14268d4710
0Pavlov <ffsvttvscsvb@gmail.com>
2024-08-19 12:54:37 +0300

    feat: Implement basic window and update loop
    This commit lays the foundation for the project's rendering by creating a black window and implementing the necessary methods for updating it. It also establishes the core structure of the main.py file.

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
-def main():
-    pass
+# noinspection PyProtectedMember
+from kivy.app import App, Builder
+from kivy.clock import Clock
+from kivy.uix.widget import Widget
+
+Builder.load_string(
+    """
+    """
+)
+
+
+class Plexus(Widget):
+    """Main class for the demo.
+
+    This class contains all the demo logic and serves as the root widget.
+    """
+
+    def __init__(self, **kwargs):
+        """Initializes the Plexus instance.
+
+        Does stuff on the app's startup.
+        """
+        super(Plexus, self).__init__(**kwargs)
+
+    def update(self, dt: float) -> None:
+        """Updates the screen."""
+        pass
+
+
+class PlexusApp(App):
+    """Main application class for the demo.
+
+    This class is responsible for setting up the game window and initializing the demo
+    logic. It inherits from Kivy's App class, which provides the main event loop and
+    window management.
+
+    Methods:
+        build(): Initializes the demo by creating an instance of Plexus and scheduling
+        its update method to be called at regular intervals.
+    """
+
+    def build(self) -> Plexus:
+        """Builds the Plexus application.
+
+        Initializes the demo and sets the update interval.
+
+        Returns:
+            Plexus: The demo instance. Root widget object.
+        """
+        root = Plexus()
+        Clock.schedule_interval(root.update, 1.0 / 120.0)
+        return root
 
 
 if __name__ == '__main__':
-    main()
+    PlexusApp().run()
