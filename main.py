8f6008d3e389031c37fc7430300d7173096a274a
0Pavlov <ffsvttvscsvb@gmail.com>
2024-08-22 13:32:22 +0300

    feat: Implement particle movement functionality
    This commit adds the move() method to the Particle() class, enabling particle movement.
    
    The move() method is called on each screen update.

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from kivy.app import App, Builder
 from kivy.clock import Clock
 from kivy.properties import (
     ListProperty,
+    NumericProperty,
 )
 from kivy.uix.widget import Widget
+from random import choice
 
 Builder.load_string(
     """
@@ -37,11 +39,13 @@ class Plexus(Widget):
         """
         super(Plexus, self).__init__(**kwargs)
         self.particle = Particle()
+        self.particle.velocity_x = choice([-3, -2, -1, 1, 2, 3])
+        self.particle.velocity_y = choice([-3, -2, -1, 1, 2, 3])
         self.add_widget(self.particle)
 
     def update(self, dt: float) -> None:
         """Updates the screen."""
-        pass
+        self.particle.move()
 
 
 class Particle(Widget):
@@ -49,8 +53,41 @@ class Particle(Widget):
 
     Attributes:
         size (ListProperty): Size of the particle in pixels.
+        velocity_x (NumericProperty): Particle's x-axis velocity.
+        velocity_y (NumericProperty): Particle's y-axis velocity.
+        pos (ListProperty): Particle's position (x, y) in pixels.
+
+    Methods:
+        move(): Moves the particle and handles collisions with the parent's boundaries.
     """
+    pos = ListProperty([0, 0])
     size = ListProperty([20, 20])
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    def move(self) -> None:
+        """Moves the particle and handles collisions with the parent's boundaries.
+
+        This method updates the particle's position based on its velocity.
+        It then checks if the particle has collided with the edges of its parent
+        widget. If a collision is detected, the particle's velocity is reversed
+        to simulate bouncing behavior.
+        """
+        # Apply velocity
+        self.pos[0] += self.velocity_x
+        self.pos[1] += self.velocity_y
+
+        # Bounce off sides
+        if self.pos[0] < self.parent.x:
+            self.velocity_x = abs(self.velocity_x)
+        elif self.pos[0] + self.size[0] > self.parent.width:
+            self.velocity_x = abs(self.velocity_x) * -1
+
+        # Bounce off top and bottom
+        if self.pos[1] < self.parent.y:
+            self.velocity_y = abs(self.velocity_y)
+        elif self.pos[1] + self.size[1] > self.parent.height:
+            self.velocity_y = abs(self.velocity_y) * -1
 
 
 class PlexusApp(App):
